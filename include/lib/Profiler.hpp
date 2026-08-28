f3080546144342bb2f505ed6f023d1bfdbc4c304
este36 <mercierstban@gmail.com>
2026-08-28 17:48:23 +0200

    feat(lib): add a RAII Profiler utility

---
+++ include/lib/Profiler.hpp
@@ -0,0 +1,19 @@
+#ifndef PROFILER_HPP
+#define PROFILER_HPP
+#include <cctime>
+
+class Profiler
+{
+	const char *_name;
+	std::clock_t _start;
+
+	// immutable
+	Profiler(const Profiler& other);
+	Profiler& operator=(const Profiler& other);
+
+public:
+	Profiler(const char *name);
+	~Profiler()
+};
+
+#endif // PROFILER_HPP
---
+++ src/lib/Profiler.cpp
@@ -0,0 +1,13 @@
+#include "Profiler.hpp"
+#include <iostream>
+
+Profiler::Profiler(const Profiler& other) { (void)other; }
+Profiler& Profiler::operator=(const Profiler& other) { (void)other; return *this; }
+
+Profiler::Profiler(const char *name) : _name(name), _start(std::clock())
+Profiler::~Profiler()
+{
+	std::clock_t end = std::clock();
+	double ms = 1000.0 * (end - _start) / CLOCKS_PER_SEC;
+	std::cout << _name << ": " << ms << " ms" << std::endl;
+}
