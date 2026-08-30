73657aa96b4131c42559542af607388bc4e0d5a4
este36 <mercierstban@gmail.com>
2026-08-31 00:23:50 +0200

    fix(URL): decode and normalize path, create URL class

--- include/URL.hpp
+++ include/URL.hpp
@@ -2,12 +2,31 @@
 #define URL_HPP
 
 #include <string>
+#include <utility>
 
 class URL
 {
+	std::string _content;
+	std::string _host;
+	std::string _path;
+	std::string _query;
+
 public:
+	URL();
+	~URL();
+	URL(const URL& other);
+	URL& operator=(const URL& other);
+
+	const std::string& getContent() const;
+	const std::string& getHost() const;
+	const std::string& getPath() const;
+	const std::string& getQuery() const;
+
 	static std::string encode(const std::string& raw);
-	static std::string decode(const std::string& encoded);
+	static std::pair<std::string, bool> decode(const std::string& s);
+	static std::pair<std::string, bool> normalize(const std::string& s);
+
+	static std::pair<URL, bool> createFromRequestTarget(const std::string& s);
 };
 
 #endif // URL_HPP
--- src/URL.cpp
+++ src/URL.cpp
@@ -1,17 +1,96 @@
 #include "URL.hpp"
 #include <sstream>
 
-std::string URL::encode(const std::string& raw)
+#include <cctype>
+#include <cctype>
+#include <exception>
+#include <iostream>
+
+URL::URL() {}
+URL::~URL() {}
+URL::URL(const URL& other)
+	: _content(other._content),
+	_host(other._host),
+	_path(other._path),
+	_query(other._query) {}
+
+URL& URL::operator=(const URL& other)
+{
+	if (this != &other)
+	{
+		_content = other._content;
+		_path = other._path;
+		_host = other._host;
+		_query = other._query;
+	}
+	return *this;
+}
+
+const std::string& URL::getContent() const { return _content; }
+const std::string& URL::getHost() const { return _host; }
+const std::string& URL::getPath() const { return _path; }
+const std::string& URL::getQuery() const { return _query; }
+
+std::pair<URL, bool> URL::createFromRequestTarget(const std::string& s)
+{
+	URL url;
+
+	url._content = s;
+
+	if (s.find('#') != std::string::npos)
+		return std::make_pair(url, false);
+
+	std::string::const_iterator start = s.begin();
+	std::string::const_iterator path_start = start;
+
+	if (s.substr(0, 4) == "http")
+	{
+		std::string::const_iterator it = start + 4;
+		if (it != s.end() && *it == 's')
+			++it;
+		if (s.end() - it < 3 || s.substr(it - start, 3) != "://")
+			return std::make_pair(url, false);
+		it += 3;
+		std::string::const_iterator auth_end = std::find(it, s.end(), '/');
+		url._host = s.substr(it - start, auth_end - it);
+		path_start = auth_end;
+	}
+	else if (s.empty() || s[0] != '/')
+		return std::make_pair(url, false);
+
+	std::string::const_iterator path_end = std::find(path_start, s.end(), '?');
+
+	if (path_end != s.end())
+		url._query = s.substr(path_end - start + 1, s.end() - path_end - 1);
+
+	std::string raw_path = s.substr(path_start - start, path_end - path_start);
+	if (raw_path.empty())
+		raw_path = "/";
+
+	std::pair<std::string, bool> decoded = URL::decode(raw_path);
+	if (!decoded.second)
+		return std::make_pair(url, false);
+
+	std::pair<std::string, bool> normalized = URL::normalize(decoded.first);
+	if (!normalized.second)
+		return std::make_pair(url, false);
+
+	url._path = normalized.first;
+
+	return std::make_pair(url, true);
+}
+
+std::string URL::encode(const std::string& s)
 {
 	static const char hex_base[] = "0123456789ABCDEF";
 
 	std::string res;
-	res.reserve(raw.length() * 3);
+	res.reserve(s.length() * 3);
 
 	char	buf[4] = {0};
 	unsigned char c;
 
-	for (std::string::const_iterator it = raw.begin(); it != raw.end(); ++it)
+	for (std::string::const_iterator it = s.begin(); it != s.end(); ++it)
 	{
 		c = *it;
 		if (std::isalnum(c) || c == '-' || c == '_'
@@ -32,49 +111,115 @@ static int hex_to_val(char c)
 {
 	if (c >= '0' && c <= '9')
 		return c - '0';
-	if (c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'f')
 		return c - 'a' + 10;
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'F')
 		return c - 'A' + 10;
 	return -1;
 }
 
-std::string URL::decode(const std::string& encoded)
+std::pair<std::string, bool> URL::decode(const std::string& s)
 {
-	std::istringstream data(encoded);
 	std::string res;
-	res.reserve(encoded.length());
+	res.reserve(s.length());
 
 	int percent_code;
 	int tmp;
 	unsigned char c;
-	unsigned char n;
 
-	while (data >> c)
+	for (std::string::const_iterator it = s.begin(); it != s.end(); ++it)
 	{
+		c = *it;
+		if (std::isspace(c))
+			return std::make_pair("", false);
 		if (c == '%')
 		{
-			data >> c;
-			tmp = hex_to_val(c);
+			if (++it == s.end())
+				return std::make_pair("", false);
+			tmp = hex_to_val(*it);
 			if (tmp == -1)
-			{
-				res += '%' + c;
-				continue;
-			}
+				return std::make_pair("", false);
 			percent_code = tmp << 4;
-
-			data >> n;
-			tmp = hex_to_val(n);
+			if (++it == s.end())
+				return std::make_pair("", false);
+			tmp = hex_to_val(*it);
 			if (tmp == -1)
-			{
-				res += '%' + c + n;
-				continue;
-			}
+				return std::make_pair("", false);
 			percent_code |= tmp;
 
 			c = static_cast<char>(percent_code);
 		}
 		res += c;
 	}
-	return res;
+	return std::make_pair(res, true);
+}
+
+std::pair<std::string, bool> URL::normalize(const std::string& s)
+{
+	if (s.find("..") == std::string::npos)
+		return std::make_pair(s, true);
+
+	std::vector<std::string> segments;
+
+	{
+		std::size_t pos = 0;
+		std::size_t end = 0;
+
+		while (true)
+		{
+			if (s[pos] == '/')
+				pos++;
+			end = s.find("/", pos);
+			if (pos != end) 
+				segments.push_back(s.substr(pos, end - pos));
+			if (end == std::string::npos)
+				break;
+			pos = end;
+		}
+
+	}
+
+	{
+		std::vector<std::string>::iterator it = segments.begin();
+		while (it != segments.end())
+		{
+			if (*it == "." || *it == "")
+				it = segments.erase(it);
+			else
+				++it;
+		}
+	}
+
+	if (segments.empty())
+		return std::make_pair("/", true);
+
+	{
+		std::vector<std::string>::iterator it = segments.begin();
+		while (it != segments.end())
+		{
+			if (*it == "..")
+			{
+				if (it == segments.begin())
+					return std::make_pair("", false);
+				it = segments.erase(it);
+				if (it == segments.begin())
+					return std::make_pair("", false);
+				--it;
+				it = segments.erase(it);
+			}
+			else
+				++it;
+		}
+	}
+
+	if (segments.empty())
+		return std::make_pair("/", true);
+
+	std::string res;
+	res.reserve(s.length());
+
+	for (std::vector<std::string>::iterator it = segments.begin(); it != segments.end(); ++it)
+		res += "/" + *it;
+
+	return std::make_pair(res, true);
 }
