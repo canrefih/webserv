6c4328ce605251cd00d6c7fd72064152c74f9489
este36 <mercierstban@gmail.com>
2026-09-11 14:23:14 +0200

    feat: add static method for getting the reason Phrase from status code

--- include/HttpResponse.hpp
+++ include/HttpResponse.hpp
@@ -25,6 +25,8 @@ class HttpResponse
 		HttpResponse();
 		~HttpResponse();
 
+		static std::string reasonPhrase(int code);
+
 		void setStatus(int code, const std::string &text); // Set the HTTP status code and corresponding status text (e.g., 200 OK, 404 Not Found)
 		void setBody(const std::string &body); // Set the body of the HTTP response, which contains the actual content to be sent to the client
 		void setContentType(const std::string &contentType); // Set the Content-Type header of the HTTP response, indicating the media type of the response body (e.g., text/html, application/json)
